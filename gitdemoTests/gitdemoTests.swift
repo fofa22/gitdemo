ad5f0500e501348167bb2dade15e632354fe4874
Fahhad Ashour <fahhadahmed.1@outlook.com>
2017-08-23 17:15:41 -0700

    Initial Commit

---
+++ gitdemoTests/gitdemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  gitdemoTests.swift
+//  gitdemoTests
+//
+//  Created by Fahhad Ashour on 8/23/17.
+//  Copyright © 2017 Fahhad Ashour. All rights reserved.
+//
+
+import XCTest
+@testable import gitdemo
+
+class gitdemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
